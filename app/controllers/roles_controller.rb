974b49c32d94f2283262d36bee903ea038abd6b4
Gizele Santos <gizelesantos@pop-os.localdomain>
2021-03-01 21:58:42 -0300

    Adicionando controller com CRUD do Role

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,34 @@
+class RolesController < ApplicationController
+
+    def index
+      render json: Role.all
+    end
+
+    def show
+      role = Role.where(id: params[:id]).first
+      if role.present?
+        render json: role
+      else
+        render json: '', status: 404
+      end 
+    end
+
+    def create
+      role = Role.create(params.require(:role).permit(:role_code, :role_name, :role_description))
+
+      render json: role, status: 201
+    end
+
+    def update
+        role = Role.find(params[:id])
+        role.update(params.require(:role).permit(:role_code, :role_name, :role_description))
+
+        render json: role, status: 200
+    end
+
+    def destroy
+        role = Role.find(params[:id])
+        role.delete
+        render json: '', status: 204
+    end
+end
